0c4b51b34d8759086147f08622a83fa711eb62ab
Lenar Khaidarov <haidarov_lenar@mail.ru>
2026-01-23 23:26:49 +0300

    add NetworkNode model

--- network/models.py
+++ network/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -27,3 +28,68 @@ class Product(models.Model):
 
     def __str__(self):
         return f"Название: {self.name}, модель: {self.model}"
+
+
+class NetworkNode(models.Model):
+    NODE_TYPES = [
+        ("factory", "Завод"),
+        ("retail", "Розничная сеть"),
+        ("entrepreneur", "Индивидуальный предприниматель"),
+    ]
+
+    name = models.CharField(max_length=200, verbose_name="Название")
+    contact = models.ForeignKey(
+        to=Contact, on_delete=models.CASCADE, verbose_name="Контакты"
+    )
+    products = models.ManyToManyField(
+        to=Product, verbose_name="Продукты", related_name="network_nodes"
+    )
+    supplier = models.ForeignKey(
+        to="self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Поставщик",
+    )
+    supplier_debt = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0.00,
+        verbose_name="Задолженность перед поставщиком",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    node_type = models.CharField(
+        max_length=12, choices=NODE_TYPES, verbose_name="Тип звена"
+    )
+    level = models.PositiveIntegerField(
+        default=0, editable=False, verbose_name="Уровень иерархии"
+    )
+
+    class Meta:
+        verbose_name = "Звено сети"
+        verbose_name_plural = "Звенья сети"
+
+    def clean(self):
+        """Валидация уровней иерархии."""
+
+        super().clean()
+
+        if self.supplier is None:
+            self.level = 0
+        else:
+            self.level = self.supplier.level + 1
+
+        if self.node_type == "factory" and self.supplier:
+            raise ValidationError("У завода не может быть поставщика")
+
+        if self.level > 2:
+            raise ValidationError(
+                f"Торговая сеть с поставщиком {self.supplier} уже имеет 3 уровня. Выберите другого поставщика"
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.get_node_type_display()}: {self.name}"
