8fa3aa1ee6c45ff465aa7c3684d7de58995c6a82
Lenar Khaidarov <haidarov_lenar@mail.ru>
2026-01-25 19:26:46 +0300

    add basic admin panel for all network models

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from network.models import NetworkNode, Product, Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [
+        "email",
+        "country",
+        "city",
+        "street",
+        "building_number",
+    ]
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "model",
+        "release_date",
+    ]
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "contact",
+        "supplier",
+        "supplier_debt",
+        "created_at",
+    ]
