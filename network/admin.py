b80d1eb60886536f278165f821482a0d3c7a1b9a
Lenar Khaidarov <haidarov_lenar@mail.ru>
2026-01-25 20:18:44 +0300

    add supplier's link for networknode in admin panel

--- network/admin.py
+++ network/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from network.models import NetworkNode, Product, Contact
 
@@ -13,6 +14,7 @@ class ContactAdmin(admin.ModelAdmin):
         "building_number",
     ]
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = [
@@ -25,10 +27,41 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
     list_display = [
-        "id",
         "name",
         "contact",
-        "supplier",
+        "supplier_link",
         "supplier_debt",
         "created_at",
     ]
+    list_filter = [
+        "contact__city",
+    ]
+    search_fields = [
+        "name",
+    ]
+    readonly_fields = [
+        "supplier_link_detailed",
+    ]
+    fields = [
+        "name",
+        "node_type",
+        "contact",
+        "products",
+        "supplier",
+        "supplier_link_detailed",
+        "supplier_debt",
+    ]
+
+    def supplier_link(self, obj):
+        if obj.supplier:
+            supplier = obj.supplier
+            url = f"/admin/network/networknode/{supplier.id}/change/"
+            return format_html('<a href="{}">{}</a>', url, supplier.name)
+        return "-"
+
+    supplier_link.short_description = "Поставщик"
+
+    def supplier_link_detailed(self, obj):
+        return self.supplier_link(obj)
+
+    supplier_link_detailed.short_description = "Ссылка на поставщика"
