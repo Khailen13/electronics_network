fd951a0daaa8eccd24f0eaafcfc6e4a8551f0f1a
Lenar Khaidarov <haidarov_lenar@mail.ru>
2026-01-27 13:33:53 +0300

    modify rest-framework authentication settings

--- electronics_network/settings.py
+++ electronics_network/settings.py
@@ -56,8 +56,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "electronics_network.wsgi.application"
 
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
