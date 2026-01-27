2be88e6831203708bb57f5beb6e313ad96ee3865
Lenar Khaidarov <haidarov_lenar@mail.ru>
2026-01-27 12:29:40 +0300

    create networknodeviewset

--- network/views.py
+++ network/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from network.filters import NetworkNodeFilter
+from network.models import NetworkNode
+from network.serializers import NetworkNodeWriteSerializer, NetworkNodeReadSerializer
+
+
+class NetworkNodeViewSet(ModelViewSet):
+    queryset = NetworkNode.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_class = NetworkNodeFilter
+
+    def get_serializer_class(self):
+        if self.action in ["create", "update", "partial_update"]:
+            return NetworkNodeWriteSerializer
+        return NetworkNodeReadSerializer
