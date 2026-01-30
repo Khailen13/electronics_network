fc58ec5ffde7cb97740ece45bff0e72b2f311283
Lenar Khaidarov <haidarov_lenar@mail.ru>
2026-01-30 16:12:26 +0300

    handle ProtectedError when deleting supplier nodes in API

--- network/views.py
+++ network/views.py
@@ -1,11 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from network.filters import NetworkNodeFilter
 from network.models import NetworkNode
 from network.permissions import IsActiveEmployee
 from network.serializers import NetworkNodeWriteSerializer, NetworkNodeReadSerializer
+from django.db.models import ProtectedError
 
 
 class NetworkNodeViewSet(ModelViewSet):
@@ -20,3 +23,12 @@ class NetworkNodeViewSet(ModelViewSet):
         if self.action in ["create", "update", "partial_update"]:
             return NetworkNodeWriteSerializer
         return NetworkNodeReadSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except ProtectedError:
+            return Response(
+                {"error": "Нельзя удалить узел, который является поставщиком для других узлов"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
